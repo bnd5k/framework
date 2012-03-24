cb4bfc650658133207ea7ede420ae910a6924b17
Ben Downey <BD@BenDowney.net>
2012-03-24 11:39:40 -0500

    updated README file

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120313130143) do
 
-  create_table "namers", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
